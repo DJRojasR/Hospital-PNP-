738bea113b3c8735a784e885ccb692e84ebb3656
Darwin Rojas <josteinrojas590@gmail.com>
2026-03-24 12:26:11 -0500

    Cambiando las especialidades y agregando assets

---
+++ Hospital PNP/src/assets/Iconos/assets.js
@@ -0,0 +1,91 @@
+import analisisclinico from './analisisclinico.png'
+import anestesiologia from './anestesiología.png'
+import asistenciasocial from './asistenciasocial.png'
+import cardiologia from './cardiologia.png'
+import cardiologia2 from './cardiologia2.png'
+import cirugia from './cirugia.png'
+import cirugiacabeza from './cirugiacabeza.png'
+import cirugiacorazon from './cirugiacorazon.png'
+import dermatologia from './dermatologia.png'
+import emergencia from './emergencia.png'
+import endocrinologia from './endocrinologia.png'
+import enfermedades from './enfermedad-infecciosa.png'
+import estomago from './estomago.png'
+import estudiotejido from './estudiotejido.png'
+import farmacia from './farmacia.png'
+import fertilidad from './fertilidad.png'
+import ginecologia from './ginecología.png'
+import hematologia from './hematologia.png'
+import inmunologia from './inmunologia.png'
+import medico from './medico.png'
+import neumologia from './neumología.png'
+import neurocirugia from './neurocirugia.png'
+import neurologia from './neurología.png'
+import nutricion from './nutricion.png'
+import nutricion2 from './nutricion2.png'
+import obstetricia from './obstetricia.png'
+import odonto from './odonto.png'
+import oftalmologia from './oftalmologia.png'
+import oncologia from './oncologia.png'
+import ortopedia from './ortopedia.png'
+import otorrinolaringologia from './otorrinolaringologia.png'
+import pediatria2 from './pediatria2.png'
+import pediatria3 from './pediatria3.png'
+import pediatria4 from './pediatria4.png'
+import pediatrica from './pediátrica.png'
+import psicologia from './psicología.png'
+import quemadura from './quemadura.png'
+import rayosx from './rayosx.png'
+import rehabilitacion from './rehabilitacion.png'
+import reumatologia from './reumatología.png'
+import sangre from './sangre.png'
+import uci from './uci.png'
+import urologia from './urologia.png'
+
+export const assets = {
+  analisisclinico,
+  anestesiologia,
+  asistenciasocial,
+  cardiologia,
+  cardiologia2,
+  cirugia,
+  cirugiacabeza,
+  cirugiacorazon,
+  dermatologia,
+  emergencia,
+  endocrinologia,
+  enfermedades,
+  estomago,
+  estudiotejido,
+  farmacia,
+  fertilidad,
+  ginecologia,
+  hematologia,
+  inmunologia,
+  medico,
+  neumologia,
+  neurocirugia,
+  neurologia,
+  nutricion,
+  nutricion2,
+  obstetricia,
+  odonto,
+  oftalmologia,
+  oncologia,
+  ortopedia,
+  otorrinolaringologia,
+  pediatria2,
+  pediatria3,
+  pediatria4,
+  pediatrica,
+  psicologia,
+  quemadura,
+  rayosx,
+  rehabilitacion,
+  reumatologia,
+  sangre,
+  uci,
+  urologia,
+}
+
+export default assets
--- Hospital PNP/src/components/Principal/Especialidades/Especialidades.jsx
+++ Hospital PNP/src/components/Principal/Especialidades/Especialidades.jsx
@@ -2,12 +2,7 @@ import React from 'react'
 import { motion } from 'framer-motion'
 import { useState } from 'react'
 import './Especialidades.css'
-import corazon from '../../../../src/assets/Iconos/cardiaco.png'
-import piel from '../../../../src/assets/Iconos/depilacion.png'
-import estomago from '../../../../src/assets/Iconos/estomago.png'
-import quemadura from '../../../../src/assets/Iconos/quemadura.png'
-import sendocrino from '../../../../src/assets/Iconos/sendocrino.png'
-import urologia from '../../../../src/assets/Iconos/urologia.png'
+import {assets} from '../../../assets/Iconos/assets.js'
  
 // especialidades agrupadas por categoría
 const categorias = [
@@ -15,72 +10,72 @@ const categorias = [
     id: 1,
     nombre: 'Especialidades Médicas',
     especialidades: [
-      { id: 1,  icon: '🤧',        nombre: 'Alergia e Inmunología',    descripcion: 'Alergias y sistema inmune.'       },
-      { id: 2,  icon: '🫀',        nombre: 'Cardiología',              descripcion: 'Enfermedades del corazón.'        },
-      { id: 3,  icon: piel,        nombre: 'Dermatología',             descripcion: 'Enfermedades de la piel.'         },
-      { id: 4,  icon: sendocrino,  nombre: 'Endocrinología',           descripcion: 'Hormonas y metabolismo.'          },
-      { id: 5,  icon: '🦠',        nombre: 'Enfermedades Infecciosas', descripcion: 'Tratamiento de infecciones.'      },
-      { id: 6,  icon: estomago,    nombre: 'Gastroenterología',        descripcion: 'Sistema digestivo.'               },
-      { id: 7,  icon: '🩸',        nombre: 'Hematología',              descripcion: 'Enfermedades de la sangre.'       },
-      { id: 8,  icon: '🩺',        nombre: 'Medicina Interna',         descripcion: 'Atención integral de adultos.'    },
-      { id: 9,  icon: '🧠',        nombre: 'Neurología',               descripcion: 'Sistema nervioso.'                },
-      { id: 10, icon: '🌬️',        nombre: 'Neumología',               descripcion: 'Sistema respiratorio.'            },
-      { id: 11, icon: '🎗️',        nombre: 'Oncología',                descripcion: 'Tratamiento del cáncer.'          },
-      { id: 12, icon: '🦵',        nombre: 'Reumatología',             descripcion: 'Enfermedades de articulaciones.'  },
+      { id: 1,  icon: assets.inmunologia,        nombre: 'Alergia e Inmunología',    descripcion: 'Alergias y sistema inmune.'       },
+      { id: 2,  icon: assets.cardiologia2,        nombre: 'Cardiología',              descripcion: 'Enfermedades del corazón.'        },
+      { id: 3,  icon: assets.dermatologia ,        nombre: 'Dermatología',             descripcion: 'Enfermedades de la piel.'         },
+      { id: 4,  icon: assets.endocrinologia,  nombre: 'Endocrinología',           descripcion: 'Hormonas y metabolismo.'          },
+      { id: 5,  icon: assets.inmunologia,        nombre: 'Enfermedades Infecciosas', descripcion: 'Tratamiento de infecciones.'      },
+      { id: 6,  icon: assets.estomago,    nombre: 'Gastroenterología',        descripcion: 'Sistema digestivo.'               },
+      { id: 7,  icon: assets.hematologia,        nombre: 'Hematología',              descripcion: 'Enfermedades de la sangre.'       },
+      { id: 8,  icon: assets.medico,        nombre: 'Medicina Interna',         descripcion: 'Atención integral de adultos.'    },
+      { id: 9,  icon: assets.neurologia,        nombre: 'Neurología',               descripcion: 'Sistema nervioso.'                },
+      { id: 10, icon: assets.neumologia,        nombre: 'Neumología',               descripcion: 'Sistema respiratorio.'            },
+      { id: 11, icon: assets.oncologia,        nombre: 'Oncología',                descripcion: 'Tratamiento del cáncer.'          },
+      { id: 12, icon: assets.reumatologia,        nombre: 'Reumatología',             descripcion: 'Enfermedades de articulaciones.'  },
     ],
   },
   {
     id: 2,
     nombre: 'Materno Infantil',
     especialidades: [
-      { id: 13, icon: '👶', nombre: 'Cirugía Pediátrica',             descripcion: 'Cirugías en niños.'           },
-      { id: 14, icon: '🏥', nombre: 'Cuidados Intensivos Pediátricos',descripcion: 'UCI pediátrica.'              },
-      { id: 15, icon: '🤰', nombre: 'Ginecología',                    descripcion: 'Salud femenina.'              },
-      { id: 16, icon: '👶', nombre: 'Medicina Pediátrica',            descripcion: 'Atención médica infantil.'    },
-      { id: 17, icon: '🍼', nombre: 'Neonatología',                   descripcion: 'Atención a recién nacidos.'   },
-      { id: 18, icon: '🤱', nombre: 'Obstetricia',                    descripcion: 'Atención del embarazo.'       },
-      { id: 19, icon: '🧬', nombre: 'Reproducción Humana',            descripcion: 'Fertilidad y reproducción.'   },
+      { id: 13, icon: assets.pediatria2, nombre: 'Cirugía Pediátrica',             descripcion: 'Cirugías en niños.'           },
+      { id: 14, icon: assets.pediatria3, nombre: 'Cuidados Intensivos Pediátricos',descripcion: 'UCI pediátrica.'              },
+      { id: 15, icon: assets.ginecologia, nombre: 'Ginecología',                    descripcion: 'Salud femenina.'              },
+      { id: 16, icon: assets.pediatria4, nombre: 'Medicina Pediátrica',            descripcion: 'Atención médica infantil.'    },
+      { id: 17, icon: assets.pediatrica, nombre: 'Neonatología',                   descripcion: 'Atención a recién nacidos.'   },
+      { id: 18, icon: assets.obstetricia, nombre: 'Obstetricia',                    descripcion: 'Atención del embarazo.'       },
+      { id: 19, icon: assets.fertilidad, nombre: 'Reproducción Humana',            descripcion: 'Fertilidad y reproducción.'   },
     ],
   },
   {
     id: 3,
     nombre: 'Especialidades Quirúrgicas',
     especialidades: [
-      { id: 20, icon: quemadura, nombre: 'Cirugía Plástica y Quemados',       descripcion: 'Reconstrucción y quemaduras.'    },
-      { id: 21, icon: '🩼',      nombre: 'Ortopedia y Traumatología',          descripcion: 'Lesiones óseas.'                 },
-      { id: 22, icon: '💉',      nombre: 'Anestesiología y Centro Quirúrgico', descripcion: 'Soporte en cirugías.'            },
-      { id: 23, icon: '🧑‍⚕️',    nombre: 'Cirugía General',                   descripcion: 'Procedimientos quirúrgicos.'     },
-      { id: 24, icon: corazon,   nombre: 'Cirugía de Tórax y Cardiovascular',  descripcion: 'Cirugía cardíaca.'               },
-      { id: 25, icon: '🧠',      nombre: 'Neurocirugía',                       descripcion: 'Cirugía del sistema nervioso.'   },
-      { id: 26, icon: '👁️',      nombre: 'Oftalmología',                       descripcion: 'Salud visual.'                   },
-      { id: 27, icon: '🗣️',      nombre: 'Cirugía de Cabeza y Cuello',         descripcion: 'Zona cabeza/cuello.'             },
-      { id: 28, icon: urologia,  nombre: 'Urología',                           descripcion: 'Sistema urinario.'               },
-      { id: 29, icon: '👂',      nombre: 'Otorrinolaringología',               descripcion: 'Oído, nariz y garganta.'         },
+      { id: 20, icon: assets.quemadura, nombre: 'Cirugía Plástica y Quemados',       descripcion: 'Reconstrucción y quemaduras.'    },
+      { id: 21, icon: assets.ortopedia,      nombre: 'Ortopedia y Traumatología',          descripcion: 'Lesiones óseas.'                 },
+      { id: 22, icon: assets.anestesiologia,      nombre: 'Anestesiología y Centro Quirúrgico', descripcion: 'Soporte en cirugías.'            },
+      { id: 23, icon: assets.cirugia,    nombre: 'Cirugía General',                   descripcion: 'Procedimientos quirúrgicos.'     },
+      { id: 24, icon: assets.cirugiacorazon,   nombre: 'Cirugía de Tórax y Cardiovascular',  descripcion: 'Cirugía cardíaca.'               },
+      { id: 25, icon: assets.neurocirugia,      nombre: 'Neurocirugía',                       descripcion: 'Cirugía del sistema nervioso.'   },
+      { id: 26, icon: assets.oftalmologia,      nombre: 'Oftalmología',                       descripcion: 'Salud visual.'                   },
+      { id: 27, icon: assets.cirugiacabeza,      nombre: 'Cirugía de Cabeza y Cuello',         descripcion: 'Zona cabeza/cuello.'             },
+      { id: 28, icon: assets.urologia,  nombre: 'Urología',                           descripcion: 'Sistema urinario.'               },
+      { id: 29, icon: assets.otorrinolaringologia,      nombre: 'Otorrinolaringología',               descripcion: 'Oído, nariz y garganta.'         },
     ],
   },
   {
     id: 4,
     nombre: 'Emergencias y Apoyo',
     especialidades: [
-      { id: 30, icon: '🚑', nombre: 'Emergencia',        descripcion: 'Atención inmediata.'        },
-      { id: 31, icon: '🏥', nombre: 'Cuidados Intensivos',descripcion: 'UCI general.'               },
-      { id: 32, icon: '🥗', nombre: 'Soporte Nutricional',descripcion: 'Apoyo alimenticio clínico.' },
+      { id: 30, icon: assets.emergencia, nombre: 'Emergencia',        descripcion: 'Atención inmediata.'        },
+      { id: 31, icon: assets.uci, nombre: 'Cuidados Intensivos',descripcion: 'UCI general.'               },
+      { id: 32, icon: assets.nutricion, nombre: 'Soporte Nutricional',descripcion: 'Apoyo alimenticio clínico.' },
     ],
   },
   {
     id: 5,
     nombre: 'Diagnóstico y Tratamiento',
     especialidades: [
-      { id: 33, icon: '👩‍⚕️', nombre: 'Asistencia Social',           descripcion: 'Apoyo social al paciente.' },
-      { id: 34, icon: '🩻',   nombre: 'Diagnóstico por Imágenes',    descripcion: 'Rayos X, TAC, etc.'        },
-      { id: 35, icon: '🩸',   nombre: 'Hemoterapia y Banco de Sangre',descripcion: 'Transfusiones.'            },
-      { id: 36, icon: '♿',   nombre: 'Medicina de Rehabilitación',  descripcion: 'Recuperación física.'      },
-      { id: 37, icon: '🍎',   nombre: 'Nutrición',                   descripcion: 'Plan alimenticio.'         },
-      { id: 38, icon: '😁',   nombre: 'Odontoestomatología',         descripcion: 'Salud bucal.'              },
-      { id: 39, icon: '🔬',   nombre: 'Patología Clínica',           descripcion: 'Análisis clínicos.'        },
-      { id: 40, icon: '🧫',   nombre: 'Anatomía Patológica',         descripcion: 'Estudio de tejidos.'       },
-      { id: 41, icon: '💭',   nombre: 'Psicología',                  descripcion: 'Salud mental.'             },
-      { id: 42, icon: '💊',   nombre: 'Farmacia',                    descripcion: 'Medicamentos.'             },
+      { id: 33, icon: assets.asistenciasocial, nombre: 'Asistencia Social',           descripcion: 'Apoyo social al paciente.' },
+      { id: 34, icon: assets.rayosx,   nombre: 'Diagnóstico por Imágenes',    descripcion: 'Rayos X, TAC, etc.'        },
+      { id: 35, icon: assets.hematologia,   nombre: 'Hemoterapia y Banco de Sangre',descripcion: 'Transfusiones.'            },
+      { id: 36, icon: assets.rehabilitacion,   nombre: 'Medicina de Rehabilitación',  descripcion: 'Recuperación física.'      },
+      { id: 37, icon: assets.nutricion2,   nombre: 'Nutrición',                   descripcion: 'Plan alimenticio.'         },
+      { id: 38, icon: assets.odonto,   nombre: 'Odontoestomatología',         descripcion: 'Salud bucal.'              },
+      { id: 39, icon: assets.analisisclinico,   nombre: 'Patología Clínica',           descripcion: 'Análisis clínicos.'        },
+      { id: 40, icon: assets.estudiotejido,   nombre: 'Anatomía Patológica',         descripcion: 'Estudio de tejidos.'       },
+      { id: 41, icon: assets.psicologia,   nombre: 'Psicología',                  descripcion: 'Salud mental.'             },
+      { id: 42, icon: assets.farmacia,   nombre: 'Farmacia',                    descripcion: 'Medicamentos.'             },
     ],
   },
 ]
